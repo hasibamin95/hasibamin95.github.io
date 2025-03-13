a16c9380a54ba9c674bbe1585a511f94610bede2
hasibamin95 <57613552+hasibamin95@users.noreply.github.com>
2025-03-13 18:40:00 +0000

    Deploying to gh-pages from @ hasibamin95/hasibamin95.github.io@8b3142ab3fce5f21b732bf7ca011896b94598e3b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -382,10 +382,12 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+    },{id: "news-announcement-1",
+          title: 'Announcement_1',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_1/";
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
